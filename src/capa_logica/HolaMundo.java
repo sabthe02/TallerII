b17892514c17ffda43314cb62e84d0654a4797dc
Madozito <juanmamadoz@gmail.com>
2025-02-06 21:36:32 -0300

    Comentario de Juanma

--- src/capa_logica/HolaMundo.java
+++ src/capa_logica/HolaMundo.java
@@ -4,5 +4,6 @@ public class HolaMundo {
 		public static void main (String args[]) {
 			System.out.println("Hola mundo");
 			System.out.println("Agrego");
+			System.out.println("Comentario de Juanma")
 		}
 }
