e6031b2b81f19f8167c7620bca38e8e4d249a7d0
santi-pereira <spereira258@gmail.com>
2025-02-20 18:53:48 -0300

    Con RMI

---
+++ src/sistema/logica/Servidor.java
@@ -0,0 +1,52 @@
+package sistema.logica;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.util.Properties;
+
+public class Servidor 
+{
+
+
+	public static void main(String args[]) 
+	{
+		
+		String ipServ = "";
+		int puerto = 0;
+		
+		try
+		{ 
+			
+			
+			Properties p = new Properties();
+			String nomArchProperties = "./config/config.properties";
+			p.load(new FileInputStream(nomArchProperties));
+			ipServ = p.getProperty("ipServidor");
+			puerto = Integer.parseInt(p.getProperty("puertoServidor"));
+
+		}
+		catch (IOException e)
+		{ e.printStackTrace(); }
+		
+		try
+		{ 
+			
+			
+			LocateRegistry.createRegistry(puerto);
+			Fachada f = new Fachada();
+			System.out.println ("Antes de publicarlo");
+			Naming.rebind("//" +ipServ+ ":" + puerto+"/fachada", f);
+			System.out.println ("Luego de publicarlo");
+		}
+		
+		catch (RemoteException e)
+		{ e.printStackTrace(); }
+		catch (MalformedURLException e)
+		{ e.printStackTrace(); }
+	}
+	
+}
