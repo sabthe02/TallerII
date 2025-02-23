296e21ba00dfff49536e9f7e721be3b2e5c8b30a
Sabine Theiss <sabthe02@web.de>
2025-02-23 19:07:46 -0300

    Agrego Respaldo al menú y también agrego controlador

--- src/sistema/grafica/VentanaRegistroMinivan.java
+++ src/sistema/grafica/VentanaRegistroMinivan.java
@@ -15,7 +15,6 @@ import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
